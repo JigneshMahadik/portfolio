c52fb707162e52165c68e4fc6cef34a020bc0307
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-09-15 02:07:50 +0530

    Banner and project has been updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,7 @@
 import "../Css/Banner.css"
 import profile from "../Images/ProfilePic.png"
+import github from "../Icons/github.svg"
+import linkedin from "../Icons/linkedin.svg"
 
 export function Banner(){
     return(
@@ -15,14 +17,16 @@ export function Banner(){
                 <h2>Hi, I'm Jigneshkumar Mahadik</h2>
                 <h3>Fullstack Developer</h3>
                 <p>
-                    Seeking an opportunity in the field of software engineering with good knowledge of various technologies.
+                    Seeking an opportunity in the field of software engineering with strong knowledge of various technologies like, Javascript, React.js, Node.js, Express.js, HTML ,CSS, MongoDB, Tailwind CSS, Redux, Git.
                     Ability to implement a full software development life cycle (SDLC) and analyze the performance of programs to
                     correct deficiencies.
                 </p>
                 {/* <!-- Buttons --> */}
                 <div class="profile-buttons">
-                    <button class="btn hire-btn">Hire Me</button>
-                    <button class="btn download-btn">Download CV</button>
+                    {/* <button class="btn hire-btn">Hire Me</button> */}
+                    {/* <button class="btn download-btn">Download CV</button> */}
+                    <a href="https://github.com/JigneshMahadik"><img src={github} className="w-12"/></a>
+                    <a href="https://www.linkedin.com/in/jigneshkumar-mahadik/"><img src={linkedin} className="w-12"/></a>
                 </div>
                 </div>
             </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,10 +8,12 @@ import pg from "../Images/pg.jpg"
 import geek from "../Images/geekfood.jpg"
 import portfolio from "../Images/portfolio.jpg"
 import "../Css/Projects.css"
+import github from "../Icons/github.svg"
+import demo from "../Icons/demo.png"
 
 export function Projects(){
     return(
-        <div>
+        <div id="projects-section">
             <h1 className="sm:text-3xl text-2xl font-medium title-font text-white">Projects & Work</h1>
             <div id="project-container">
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -20,12 +22,18 @@ export function Projects(){
                     </div>
                     <div class="p-4">
                         <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">Fully-fledged real-estate web app</h2>
+                        {/* <h2 class="mb-2 text-base font-medium dark:text-white text-gray-900">Key Features : Property posting, request, AI Bot, JWT authentication, Mail service</h2> */}
                         {/* <p class="text-base dark:text-gray-300 text-gray-700">Product description goes here.</p> */}
                         {/* <div class="flex items-center">
                         <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <a href=""><img src={github} className="w-7"/></a>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -38,8 +46,14 @@ export function Projects(){
                         <div class="flex items-center">
                         <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
-                        <p class="ml-auto text-base font-medium text-green-500">20% off</p>
+                        <p class="ml-auto text
+                        -base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -54,6 +68,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -68,6 +87,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -80,8 +104,14 @@ export function Projects(){
                         <div class="flex items-center">
                         <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
-                        <p class="ml-auto text-base font-medium text-green-500">20% off</p>
+                        <p class="ml-auto text-
+                        base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -96,6 +126,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -110,6 +145,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -124,6 +164,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
                 <div id="pro-card" class="mx-auto mt-11 transform overflow-hidden bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
@@ -138,6 +183,11 @@ export function Projects(){
                         <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
                         <p class="ml-auto text-base font-medium text-green-500">20% off</p>
                         </div> */}
+                        <div className="flex justify-between gap-3 align-middle">
+                            <img src={github} className="w-7"/>
+                            <p className="font-bold text-white" id="demo">Demo</p>
+                            {/* <img src={demo} className="w-4 h-4"/> */}
+                        </div>
                     </div>
                 </div>
             </div>
